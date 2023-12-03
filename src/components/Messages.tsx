81a213a8b2ec60a9d1445a6182b8720d0e9ff7a1
Ugo Porcu <ugo.porcu27@gmail.com>
2023-12-03 22:49:04 +0100

    🔄 chore(dashboard/page.tsx): update import path for ChatWrapper component to reflect new location in components/chat directory
    🔥 chore(ChatWrapper.tsx): remove unused ChatWrapper component
    
    ✨ feat(Messages.tsx): add Messages component to handle displaying chat messages
    
    ✨ feat(chat/ChatInput.tsx): add ChatInput component to handle user input in chat
    
    ✨ feat(chat/ChatWrapper.tsx): create ChatWrapper component to handle chat functionality, including displaying messages and user input
    
    🔄 chore(trpc/index.ts): add getFileUploadStatus query to retrieve file upload status from the database

---
+++ src/components/Messages.tsx
@@ -0,0 +1,5 @@
+const Messages = () => {
+    return <div>msg</div>;
+};
+
+export default Messages;
---
+++ src/components/chat/ChatInput.tsx
@@ -0,0 +1,7 @@
+const ChatInput = () => {
+    return <div>
+        ss
+    </div>
+};
+
+export default ChatInput;
