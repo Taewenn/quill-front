87866e3f2cd8496cfbc8c41fb086074c4d365ccf
Ugo Porcu <ugo.porcu27@gmail.com>
2023-12-03 19:28:17 +0100

    🚀 feat(package.json): add new dependencies to support file upload functionality 🚀 feat(server.ts): add new route and middleware for file upload functionality 🚀 feat(app/api/uploadthing/core.ts): create core file upload functionality 🚀 feat(app/api/uploadthing/route.ts): create route for file upload functionality 🚀 feat(app/dashboard/[fileid]/page.tsx): create new page for displaying uploaded file 🚀 feat(app/layout.tsx): add new component for displaying toasts 🚀 feat(components/ChatWrapper.tsx): create new component for chat functionality 🚀 feat(components/PdfRenderer.tsx): create new component for rendering PDF files 🚀 feat(components/UploadButton.tsx): add new component for uploading files 🚀 feat(components/ui/progress.tsx): add new component for displaying progress bar 🚀 feat(components/ui/toast.tsx): add new components for displaying toasts 🚀 feat(components/ui/toaster.tsx): add new component for managing toasts 🚀 feat(components/ui/use-toast.ts): add new hook for managing toasts 🚀 feat(lib/uploadthing.ts): add new hook for using uploadthing functionality 🚀 feat(trpc/index.ts): add new endpoint for fetching file by key

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,9 @@ import { cn } from "@/lib/utils";
 
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
+import { Toaster } from "@/components/ui/toaster";
 
-import "react-loading-skeleton/dist/skeleton.css"
+import "react-loading-skeleton/dist/skeleton.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,6 +32,7 @@ export default function RootLayout({
                 >
                     <Navbar />
                     {children}
+                    <Toaster />
                 </body>
             </Providers>
         </html>
