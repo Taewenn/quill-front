965e03ad839b664228826bc1eb400fd154775649
Ugo Porcu <ugo.porcu27@gmail.com>
2023-12-03 22:28:48 +0100

    🐛 fix(next.config.js): add webpack configuration to disable canvas and encoding aliases to improve build performance and avoid unnecessary dependencies 🚀 feat(package.json): add new dependencies @hookform/resolvers, @radix-ui/react-dropdown-menu, react-hook-form, react-resize-detector, and simplebar-react to enhance form functionality and UI components 🐛 fix(page.tsx): pass file.url as a prop to PdfRenderer component to render the PDF correctly 🐛 fix(layout.tsx): move import of globals.css and simplebar-react.css to the top to ensure proper styling and avoid conflicts 🚀 feat(PdfFullScreen.tsx): add new component to render PDF in fullscreen mode with support for zooming, rotating, and navigating pages 🚀 feat(PdfRenderer.tsx): add new component to render PDF with support for zooming, rotating, and navigating pages 🚀 feat(ui/dropdown-menu.tsx): add new component to create dropdown menus with various options and styles 🚀 feat(ui/input.tsx): add new component to create input fields with consistent styling and behavior

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { cn } from "@/lib/utils";
 
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
 import { Toaster } from "@/components/ui/toaster";
 
+import "./globals.css";
 import "react-loading-skeleton/dist/skeleton.css";
+import "simplebar-react/dist/simplebar.min.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
